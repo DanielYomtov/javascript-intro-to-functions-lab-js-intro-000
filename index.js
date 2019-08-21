bc0b9a6ed10dc415cf0154d307c54b28c75a6571
daniel yomtov <yomtovdaniel1@gmail.com>
2019-08-21 12:05:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ function whisper(string) {
 }
 
 function sayHiToGrandma(string){
-  if (string === string.toLowerCase) {
+  if (string.toLowerCase === string) {
     return "I can't hear you!";
-  } else if (string === string.toUpperCase){
+  } else if (string.toUpperCase === string){
     return "YES INDEED!";
   } else if (string === "I love you grandma"){
-    return "I love you, too";
+    return "I love you, too.";
   }
 }

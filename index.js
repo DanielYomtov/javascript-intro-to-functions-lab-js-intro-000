845f3eeb3874dbc8be681f8f25370ecab8a9700d
daniel yomtov <yomtovdaniel1@gmail.com>
2019-08-21 11:58:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ function whisper(string) {
   return string.toLowerCase();
 }
 
-function logshout(string) {
-  console.log(string.toUpperCase);
+function sayHiToGrandma(string){
+  if(string === string.toUpperCase){
+     return "Hi";
+  } else if (string === string.toLowerCase) {
+    return "i cant hear you";
+  } else {
+    return "I love you, Grandma.";
+  }
 }

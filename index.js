0e2aa7b59f4144a7d439c27d73d7f3649487659e
daniel yomtov <yomtovdaniel1@gmail.com>
2019-08-21 12:08:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function logshout(string){
+function logShout(string){
   console.log(string.toUpperCase());
 }
 
-function whisper(string) {
-  return string.toLowerCase();
+function logWhisper(string) {
+  console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string){
